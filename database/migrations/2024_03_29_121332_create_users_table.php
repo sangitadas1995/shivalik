e3fb80504be9f97b0e2012afb22e98bd8391fc94
Rakesh7741 <rakesh@maxbridgesolution.com>
2024-03-29 17:49:50 +0530

    add user migration file

---
+++ database/migrations/2024_03_29_121332_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('manager_id')->nullable();
+            $table->integer('func_area_id')->nullable();
+            $table->integer('designation_id')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            $table->string('address')->nullable();
+            $table->integer('country_id')->nullable();
+            $table->integer('state_id')->nullable();
+            $table->integer('city_id')->nullable();
+            $table->integer('pincode')->nullable();
+            $table->enum('status', ['A', 'I', 'D'])->default('A');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
