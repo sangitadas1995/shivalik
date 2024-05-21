e4d0df56d7abe50d96069ff1387cdb87ec044bd4
sngitamaxbridge <54091434+sangitadas1995@users.noreply.github.com>
2024-05-21 17:02:11 +0530

    changes admin terms and condition

--- resources/views/settings/adminTermSettings/admin-settings.blade.php
+++ resources/views/settings/adminTermSettings/admin-settings.blade.php
@@ -7,7 +7,7 @@
    <div class="row justify-content-between align-items-center">
       <div class="col-md-4">
          <h2>
-            <a href="#"><i class="ri-arrow-left-line"></i></a> Edit Payment Terms
+            <a href="#"><i class="ri-arrow-left-line"></i></a> Edit Admin Terms & Condition
          </h2>
       </div>
    </div>
